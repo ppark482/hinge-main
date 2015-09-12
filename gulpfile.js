6fb36645ed7fbd391af6394fe4df81fe54130880
rdanieldesign <rdanieldesign@gmail.com>
2015-09-12 11:08:09 -0400

    add more gulp modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,30 @@
 var 	gulp = require('gulp'),
 			serve = require('gulp-webserver'),
-			connect = require('gulp-connect');
+			connect = require('gulp-connect'),
+			sass = require('gulp-sass'),
+			concat = require('gulp-concat'),
+			uglify = require('gulp-uglify');
 
-gulp.task('default', ['connect', 'serve']);
+gulp.task('default', ['js', 'sass', 'html', 'connect', 'serve', 'watch']);
+
+gulp.task('sass', function () {
+  gulp.src('./assets/css/main.scss')
+    .pipe(sass().on('error', sass.logError))
+		.pipe(concat('main.css'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('./assets/*.js')
+    .pipe( concat('main.js'))
+    .pipe( uglify() )
+    .pipe( gulp.dest('./dist/'));
+});
+
+gulp.task('html', function() {
+  return gulp.src(['./index.html'])
+    .pipe( gulp.dest('./dist/'));
+});
 
 gulp.task('serve', function(){
 	serve({
@@ -18,5 +40,7 @@ gulp.task('connect', function(){
 });
 
 gulp.task('watch', function () {
-
+  gulp.watch('./assets/css/*.scss',['sass']);
+  gulp.watch('./assets/js/*.js',['js']);
+  gulp.watch('./index.html',['html']);
 });
