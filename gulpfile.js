335266524c5dae2cf33bac2e030d2a4b8963c73d
Paul Park <ppark482@gmail.com>
2015-09-11 21:08:59 -0400

    removed sections, added logo

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var 	gulp = require('gulp'),
-			serve = require('gulp-webserver'),
-			connect = require('gulp-connect');
+		serve = require('gulp-webserver'),
+		connect = require('gulp-connect'),
+		sass = require('gulp-sass');
 
 gulp.task('default', ['connect', 'serve']);
+
+gulp.task('sass', function () {
+  gulp.src('./assets/css/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css/'));
+});
 
 gulp.task('serve', function(){
 	serve({
