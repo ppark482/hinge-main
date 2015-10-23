93c98677f101b53ab07b0bf29a706eb2590b907e
rdanieldesign <rdanieldesign@gmail.com>
2015-10-22 22:25:32 -0400

    Add simple form submission

--- components/contact-form/ContactDirective.js
+++ components/contact-form/ContactDirective.js
@@ -4,9 +4,9 @@
 		.module('app')
 		.directive('contactForm', ContactDirective);
 
-	ContactDirective.$inject = ['AssetService'];
+	ContactDirective.$inject = ['AssetService', '$http'];
 
-	function ContactDirective (AssetService) {
+	function ContactDirective (AssetService, $http) {
 		var directive = {
 			link: link,
 			templateUrl: '/templates/components/contact-form/contact-form.html',
@@ -20,6 +20,31 @@
 
 		function link (scope, element, attrs) {
 
+			var message = {
+				name: scope.senderName,
+				company: scope.company,
+				email: scope.email,
+				phone: scope.phone,
+				message: scope.message
+			};
+
+			var showSuccessMessage = function(data){
+				console.log(data);
+				alert('success');
+			};
+
+			var showFailureMessage = function(){
+				alert('failure');
+			};
+
+			scope.submitForm = function(){
+				$http({
+				    url: "//formspree.io/you@email.com",
+				    method: "POST",
+				    data: message,
+				    dataType: "json"
+				}).then( showSuccessMessage, showFailureMessage );
+			};
 		}
 	}
 
